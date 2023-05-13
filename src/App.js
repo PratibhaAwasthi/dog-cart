e15680e9807a8f7ad7206c70c4b75d34d4b2fac7
PratibhaAwasthi <pratibhaawasthi17@gmail.com>
2023-05-13 16:41:43 -0700

    created components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Products from "./Products";
+import SingleProducts from "./SingleProduct";
+import Cart from "./Cart";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/singleproduct/:id" element={<SingleProduct />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/SingleProduct.js
@@ -0,0 +1,3 @@
+const SingleProduct = () => {
+  return <Wrapper></Wrapper>;
+};
