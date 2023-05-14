58b7b5fdf104af6d0fb4293317b6237998d723fa
PratibhaAwasthi <pratibhaawasthi17@gmail.com>
2023-05-13 23:20:03 -0700

    introduced new pages

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
+import styled from "styled-components";
+import HeroSection from "./components/HeroSection";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <HeroSection />;
 };
 
 export default Home;
